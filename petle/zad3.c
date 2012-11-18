d417bd599bffd42711458aab85b2d2eb70570b91
Igor Kablasz <igor.kablasz17@gmail.com>
2012-11-18 10:55:07 +0100

    zadanie 3

---
+++ petle/zad3.c
@@ -0,0 +1,26 @@
+// Zadanie 3
+
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+
+  printf("podaj pierwsza liczbe\n");
+  scanf("%d",&a);
+  printf("podaj druga liczbe\n");
+  scanf("%d",&b);
+
+  if(a<b)
+    {
+      for(a=a;a<=b;a++)
+	{
+	  printf("%d ",a);
+	}
+      printf("\n");
+    }
+  else
+    {
+      printf("Podałeś zły zakres!\n");
+    }
+}
