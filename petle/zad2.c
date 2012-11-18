482a0d654a9e922e55ba8d18d3447025e2ff8585
Igor Kablasz <igor.kablasz17@gmail.com>
2012-11-18 10:25:09 +0100

    poprawki w wyswietlaniu

--- petle/zad2.c
+++ petle/zad2.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 int main() {
-  int x = 1;
+  int x = 2;
   
   printf("kolejne potegi liczby 2 nie przekraczajace 2010:\n");
-  
+  printf("%d", 1);
   while (x <= 2010) {
-    printf("%d, ",x);
+    printf(", %d",x);
     x *=2;
   }
   printf("\n");
