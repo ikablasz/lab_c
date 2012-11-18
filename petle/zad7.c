6c0a9e8c26759227e6cb42628f18cac2431fc429
Igor Kablasz <igor.kablasz17@gmail.com>
2012-11-18 11:58:49 +0100

    niedokonczone 7 zadanie

---
+++ petle/zad7.c
@@ -0,0 +1,18 @@
+//zadanie 6
+#include <stdio.h>
+
+int main()
+{
+  double tab[6];
+  int i;
+
+  for(i=0;i<6;i++)
+    {
+      printf("Podaj %d. liczbę\n",i+1);
+      scanf("%lf",&tab[i]);
+    }
+  if (tab[i] > 0){
+    printf("Liczba dodatnich liczb w tablicy to: %lf",tab[i]);
+  }
+  
+}
