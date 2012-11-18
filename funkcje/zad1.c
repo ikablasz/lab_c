db0bb3d0cc84a815a951e9295602b1695a2468ec
Igor Kablasz <igor.kablasz17@gmail.com>
2012-11-18 12:34:25 +0100

    zadanie pierwsze z funkcji

---
+++ funkcje/zad1.c
@@ -0,0 +1,19 @@
+//zadanie 6
+#include <stdio.h>
+#include <math.h>
+
+double przeciw(double a,double b){
+  return sqrt((a*a)+(b*b));
+}
+
+int main()
+{
+  double a,b;
+
+  printf("podaj dl 1 przyprost\n");
+  scanf("%lf",&a);
+  printf("podaj dl 2 przyprostokatnej\n");
+  scanf("%lf",&b);
+  
+  printf("Dl przeciwprostokatnej tego trojkata to :%.2lf\n",przeciw(a,b)); 
+}
