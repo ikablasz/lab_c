52e087c179719e5ea3d775be9eb3f8cc7e76ec11
ikablasz <igor.kablasz17@gmail.com>
2012-11-25 09:12:41 +0100

    Update petle/zad2.c

--- petle/zad2.c
+++ petle/zad2.c
@@ -2,20 +2,15 @@
 #include <stdio.h>
 
 
-int main()
-{
+int main(){
   int i,j;
   
-  for(i=1;i<9;i++)
-    {
-      for(j=1;j<9;j++)
-	{
-	  if(i%2!=0)
-	    {
+  for(i=1;i<9;i++){
+      for(j=1;j<9;j++){
+	  if(i%2!=0){
 	      printf("* ");
 	    }
-	  else
-	    {
+	  else{
 	      printf(" *");
 	    }
 	}
