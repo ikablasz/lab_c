cd6696a21fc763b5bef7a5adba40193438f3eaba
Igor Kablasz <igor.kablasz17@gmail.com>
2012-11-25 12:45:28 +0100

    poprawiłem składnię

--- funkcje/zad4.c
+++ funkcje/zad4.c
@@ -1,27 +1,30 @@
 // zadanie 4
+
 #include <stdio.h>
 
-
-int change(int n)
-{
-  char bufor[20], bufor2[20];
-  int dlugosc, i;
-  
-  snprintf(bufor, 20, "%d",n);
-  dlugosc=strlen(bufor);
-
-  for(i=0; i<= dlugosc-1; i++){
-      bufor2[i] = bufor[dlugosc-1-i];
-    }
-  
-  sscanf(bufor2, "%d", &i);
-
-  return i;
-}
+int zamiana(int n);
 
 int main(){
   int a;
+
   printf("podaj liczbę\n");
   scanf("%d", &a);
-  printf("Podana liczba: %d. Po zmianie: %d\n", a, change(a));
+  printf("Podana liczba: %d. Po zmianie: %d\n", a, zamiana(a));
+}
+
+int zamiana(int n){
+
+  char bufor[20], bufor2[20];
+  int dl, i;
+
+  snprintf(bufor, 20, "%d",n);
+  dl=strlen(bufor);
+
+  for(i=0; i<= dl-1; i++){
+    bufor2[i] = bufor[dl-1-i];
+  }
+
+  sscanf(bufor2, "%d", &i);
+
+  return i;
 }
