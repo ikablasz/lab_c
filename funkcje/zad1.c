f5eda1158340305fa4e54300568b0d2130211cb4
Igor Kablasz <igor.kablasz17@gmail.com>
2012-11-25 12:15:51 +0100

    poprawiłem składnię

--- funkcje/zad1.c
+++ funkcje/zad1.c
@@ -1,20 +1,22 @@
-//zadanie 1 Oblicza przeciwprostokątną 
+//zadanie 1 - Oblicza przeciwprostokątną 
 
 #include <stdio.h>
 #include <math.h>
 
-double przeciw(double a,double b){
-  return sqrt((a*a)+(b*b));
-}
+double przeciw(double a,double b);  
 
 int main()
 {
   double a,b;
 
-  printf("podaj dl 1 przyprost\n");
+  printf("podaj długość 1 przyprost\n");
   scanf("%lf",&a);
-  printf("podaj dl 2 przyprostokatnej\n");
+  printf("podaj długość 2 przyprostokatnej\n");
   scanf("%lf",&b);
   
-  printf("Dl przeciwprostokatnej tego trojkata to :%.2lf\n",przeciw(a,b)); 
+  printf("Dlugość przeciwprostokatnej tego trojkata to: %.2lf\n",przeciw(a,b));
+}
+
+double przeciw(double a, double b){
+  return sqrt((a*a)+(b*b));
 }
