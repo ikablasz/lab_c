8d85adfb655015ff44239ee0ac96c27e8c3b45ba
ikablasz <igor.kablasz17@gmail.com>
2012-12-08 20:39:14 +0100

    Update funkcje/zad6.c

--- funkcje/zad6.c
+++ funkcje/zad6.c
@@ -2,56 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 
-int spraw(int a, int b, int wynik) 
-{
-  if(wynik==a*b)
-    {
+int spraw(int a, int b, int wynik) {
+  if(wynik==a*b){
       return 1;
-    }
-  else
-    {
+  }
+    else{
       return 0;
     }
 }
 
-int rnd(int min, int max)
-{
+int rnd(int min, int max){
   int tmp;
   if (max>=min)
     max-= min;
-  else
-    {
-      tmp= min - max;
-      min= max;
-      max= tmp;
-    }
+      else{
+       tmp= min - max;
+       min= max;
+       max= tmp;
+      }
   return max ? (rand() % max + min) : min;
 }
 
-void mainMenu()
-{
+void mainMenu(){
+	
   int wyjscie=0,wynik,a,b,zla_odp=0,dobra_odp=0;
 
   printf("Program sprawdzi czy potrafisz mnozyc.\n");
   
-  while(wyjscie!=5)
-    {
+  while(wyjscie!=5){
+  	
       printf("\n");
+      
       a=rnd(1,10);
       b=rnd(1,10);
+      
       printf("Ile to jest %d * %d ?\n",a,b);
       scanf("%d",&wynik);
 
-      if(spraw(a,b,wynik)==1)
-      {
+      if(spraw(a,b,wynik)==1){
+      	
         printf("Dobra odpowiedz !!!\n");
 	  dobra_odp++;
-	  }
-      else
-      {
-        printf("Zla odpowiedz !!!\n");
-	  zla_odp++;
-	  }
+      }
+         else{
+           printf("Zla odpowiedz !!!\n");
+	    zla_odp++;
+	 }
 
       wyjscie++;
     }
@@ -61,7 +57,6 @@ void mainMenu()
   printf("Podsumowanie: %d Dobrych odpowiedzi, %d Zlych odpowiedzi\n",dobra_odp,zla_odp);
 }
 
-int main()
-{
+int main(){
   mainMenu();
 }
