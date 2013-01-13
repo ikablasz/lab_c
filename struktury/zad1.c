662b6af2735bd52baae1f6120b20b83127dd0613
Igor Kablasz <igor.kablasz17@gmail.com>
2013-01-13 09:52:45 +0100

    zadanie 1

--- struktury/zad1.c
+++ struktury/zad1.c
@@ -5,35 +5,30 @@ struct point {
   int y;
 };
 
-struct rect
-{
+struct rect{
   struct point ll; //lower left
   struct point ur; //lower right
 };
 
-struct point makepoint(int x, int y)
-{
+struct point makepoint(int x, int y){
   struct point tmp;
   tmp.x=x;
   tmp.y=y;
   return tmp;
 };
 
-int ptinrect(struct point p, struct rect r)
-{
+int ptinrect(struct point p, struct rect r){
   return p.x >= r.ll.x && p.x < r.ur.x &&p.y >= r.ll.y && p.y < r.ur.y;
 }
 
-int rectinrect(struct rect r1, struct rect r2) 
-{
+int rectinrect(struct rect r1, struct rect r2) {
   if(ptinrect(r1.ll,r2) == 1 && ptinrect(r1.ur,r2) == 1)
     return 1;
   else
     return 0;
 }
 
-int main()
-{
+int main(){
   struct rect test1, test2;
 
   test1.ll=makepoint(0,0);
