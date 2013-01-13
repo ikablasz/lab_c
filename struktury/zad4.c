840ad09cf447929873122c568d8f0b104becac28
Igor Kablasz <igor.kablasz17@gmail.com>
2013-01-13 11:55:35 +0100

    niedokończone zadanie 4

---
+++ struktury/zad4.c
@@ -0,0 +1,52 @@
+//zadanie 4
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point {
+  int x;
+  int y;
+};
+
+struct rect
+{
+  struct point ll; //lower left
+  struct point ur; //upper right
+};
+
+struct point makepoint(int x, int y)
+{
+  struct point tmp;
+  tmp.x=x;
+  tmp.y=y;
+  return tmp;
+};
+
+int ptinrect(struct point p, struct rect r){
+  return p.x >= r.ll.x && p.x < r.ur.x &&p.y >= r.ll.y && p.y < r.ur.y;
+}
+
+int disjointrect(struct rect r1, struct rect r2){
+  if( (r1.ll.x > r2.ur.x || r1.ur.x < r2.ll.x) && ( r1.ll.y > r2.ur.y || r2.ll.y > r1.ur.y) )
+    return 1;
+  else
+    return 0;
+}
+
+int rectinrect(struct rect r1, struct rect r2){
+  if(ptinrect(r1.ll,r2) == 1 && ptinrect(r1.ur,r2) == 1)
+    return 1;
+  else
+    return 0;
+}
+
+int area(struct rect r1,struct rect r2)
+{
+  int area=0,area2=0;
+
+  area=( abs(r1.ur.x-r1.ll.x)
+
+int main(){
+
+
+}
